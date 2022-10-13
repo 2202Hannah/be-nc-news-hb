38f5b6468db0180b63626b078f5c6009aded9f39
2202Hannah <hannahbarber22@hotmail.co.uk>
2022-10-13 14:58:50 +0100

    remove console.log

--- controllers/comments.controller.js
+++ controllers/comments.controller.js
@@ -2,7 +2,7 @@ const { deleteComment } = require(`../models/comments.model`);
 
 exports.removeComment = (request, response, next) => {
   const { comment_id } = request.params;
-  console.log("In the controller");
+
   deleteComment(comment_id)
     .then(() => {
       response.status(204).send();
