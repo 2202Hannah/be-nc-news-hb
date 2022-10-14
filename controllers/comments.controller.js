0c24591d8d494e0449a1c792f0cae0a7c74901a9
2202Hannah <hannahbarber22@hotmail.co.uk>
2022-10-14 14:00:59 +0100

    add new endpoint PATCH /api/comments/:commend_id

--- controllers/comments.controller.js
+++ controllers/comments.controller.js
@@ -1,4 +1,7 @@
-const { deleteComment } = require(`../models/comments.model`);
+const {
+  deleteComment,
+  updateCommentVotes
+} = require(`../models/comments.model`);
 
 exports.removeComment = (request, response, next) => {
   const { comment_id } = request.params;
@@ -6,6 +9,20 @@ exports.removeComment = (request, response, next) => {
   deleteComment(comment_id)
     .then(() => {
       response.status(204).send();
+    })
+    .catch(err => {
+      next(err);
+    });
+};
+
+exports.patchCommentVotes = (request, response, next) => {
+  const { comment_id } = request.params;
+  const { inc_votes: votes } = request.body;
+
+  updateCommentVotes(comment_id, votes)
+    .then(comment => {
+      console.log(comment);
+      response.status(200).send({ comment });
     })
     .catch(err => {
       next(err);
